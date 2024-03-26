e2c1f63e9eb2ac8f921f3ed4b6656f332620f8a2
greendream21 <uri.dev.0802@gmail.com>
2024-03-26 07:28:41 -0400

    # fix for building success

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,15 @@
 import Image from "next/image";
-import { AI } from '@/lib/chat/actions'
+import { AI } from "@/lib/chat/actions";
 import { nanoid } from "ai";
-import { auth } from '@/auth'
+// import { auth } from '@/auth'
 import { Chat } from "@/components/chat/chat";
 
-
 export default function Home() {
-  const id = nanoid()
+  const id = nanoid();
 
   return (
     <AI initialAIState={{ chatId: id, messages: [] }}>
-    <Chat id={id} session={undefined} missingKeys={[]} />
+      <Chat id={id} session={undefined} missingKeys={[]} />
     </AI>
   );
 }
